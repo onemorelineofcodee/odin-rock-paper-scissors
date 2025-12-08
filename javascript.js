aa65ddaf68d877bb7fbb06ce76d554ed45b499f8
onemorelineofcodee <ajkanuga@gmail.com>
2025-12-08 22:11:46 +0530

    complete playrond function
    completed all the conditions for playround function

--- javascript.js
+++ javascript.js
@@ -17,11 +17,25 @@ function getComputerChoice(){
 }
 
 
-function playRound(humanChoice=getHumanChoice(),ComputerChoice=getComputerChoice()){
+function playRound(humanChoice=getHumanChoice(),computerChoice=getComputerChoice()){
     let humanScore = 0 ,computerScore = 0 ;
-    if (humanChoice == ComputerChoice){
-        console.log(`it's a tie between ${humanChoice} and ${ComputerChoice}`);
-    } 
+    if (humanChoice == computerChoice){
+        console.log(`it's a tie between ${humanChoice} and ${computerChoice}`);
+    } else if (humanChoice=="rock" && computerChoice=="paper"){
+        console.log("you lose!! paper beats rock");
+    } else if (humanChoice=="rock" && computerChoice=="paper") {
+        console.log("you win!! rock beats paper ");
+    } else if (humanChoice=="paper" && computerChoice=="rock") {
+        console.log("you win!! paper beats rock");
+    } else if (humanChoice=="paper" && computerChoice=="scissor") {
+        console.log("you lose!! scissor beats paper ");
+    } else if (humanChoice=="scissor" && computerChoice=="rock") {
+        console.log("you lose!! scissor beats rock");
+    } else if (humanChoice=="scissor" && computerChoice=="paper") {
+        console.log("you win!! scissor beats paper ");
+    }    
     
 }
+
+playRound();
 
